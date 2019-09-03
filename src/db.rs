37f8bcf900333d34ea909c4783159739fd05c415
axion014 <axion014@gmail.com>
2019-09-03 16:52:26 +0900

    separate db operations

---
+++ src/db.rs
@@ -0,0 +1,187 @@
+use actix_web::{web, error, http::StatusCode, Error as ActixError};
+use std::io::{self, ErrorKind};
+use futures_locks::Mutex;
+use serde_json::Value;
+use std::error::Error;
+use actix::prelude::*;
+use tokio_postgres::{connect, NoTls, Statement, Client, Row, types::{Type, Kind, IsNull, ToSql}};
+use futures::future;
+
+#[derive(Debug)]
+pub enum ActorVariant {
+	User,
+	Group
+}
+
+// Manually expanded https://github.com/sfackler/rust-postgres-derive since it didn't work with tokio-postgres
+impl ToSql for ActorVariant {
+	fn to_sql(&self, _type: &Type, buf: &mut Vec<u8>) -> Result<IsNull, Box<Error + Sync + Send>> {
+		let s = match self {
+        	ActorVariant::User => "member",
+			ActorVariant::Group => "organization"
+    	};
+
+    	buf.extend_from_slice(s.as_bytes());
+    	Ok(IsNull::No)
+	}
+
+    fn accepts(type_: &Type) -> bool {
+		if type_.name() != "actors_available" {
+            return false;
+        }
+
+        match *type_.kind() {
+            Kind::Enum(ref variants) => {
+                if variants.len() != 2 {
+                    return false;
+                }
+
+                variants.iter().all(|v| {
+                    match &**v {
+                        "member" => true,
+                        "organization" => true,
+                        _ => false
+                    }
+                })
+            }
+            _ => false
+        }
+    }
+
+    fn to_sql_checked(&self, type_: &Type, buf: &mut Vec<u8>) -> Result<IsNull, Box<Error + Sync + Send>> {
+		self.to_sql(type_, buf)
+	}
+}
+
+pub fn into_value(row: &Row, name: &str, col_type: &Type) -> Value {
+	macro_rules! from_sql {
+		($(($sql_type:ident, $type_to:ty)),*) => {
+			match col_type {
+				$(&Type::$sql_type => row.get::<&str, $type_to>(name).into(),)*
+				_ => panic!("Specified SQL cell's type is not compatible to JSON")
+			}
+		}
+	}
+	from_sql![
+		(CHAR, i8),
+		(INT2, i16),
+		(INT4, i32),
+		(INT8, i64),
+		(OID, u32),
+		(FLOAT4, f32),
+		(FLOAT8, f64),
+		(BYTEA, &[u8]),
+		(TEXT, &str),
+		(BOOL, bool)
+	]
+}
+
+pub struct Db {
+	client: Client,
+	statements: Statements
+}
+
+impl Actor for Db {
+    type Context = Context<Self>;
+}
+
+impl Db {
+	pub fn get(&mut self) -> (&mut Client, &Statements) {
+		(&mut self.client, &self.statements)
+	}
+}
+
+pub struct Statements {
+	pub get_inbox: Statement,
+	pub get_outbox: Statement,
+	pub create_message: Statement,
+	pub add_sender: Statement,
+	pub add_reciever: Statement,
+	pub create_group: Statement,
+	pub delete_group: Statement
+}
+
+pub type DbWrapper = web::Data<Mutex<Db>>;
+
+pub fn process_senders(json: Value, id: i64, db: DbWrapper) -> Box<Future<Item = (), Error = ActixError>> {
+	match json {
+		Value::String(str) => {
+			Box::new(db.lock().from_err().join(future::ok(str)).and_then(move |(mut db_locked, str)| {
+				let (client, statements) = db_locked.get();
+				client.execute(&statements.add_sender, &[&id, &str])
+					.map(|_| ()).map_err(error::ErrorInternalServerError)
+			}))
+		},
+		Value::Object(obj) => {
+			Box::new(db.lock().from_err().join(future::ok(obj)).and_then(move |(mut db_locked, obj)| {
+				let (client, statements) = db_locked.get();
+				client.execute(&statements.add_sender, &[&id, &obj["id"].as_str()])
+					.map(|_| ()).map_err(error::ErrorInternalServerError)
+			}))
+		},
+		Value::Array(arr) => Box::new(
+			future::join_all(arr.to_owned().into_iter().map(move |el| process_senders(el, id, db.clone()))).map(|_| ())
+				.map_err(|e| error::InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR).into())
+		),
+		_ => Box::new(future::err(error::ErrorBadRequest("Invaild actor")))
+	}
+}
+
+pub fn process_recievers(json: Value, id: i64, db: DbWrapper) -> Box<Future<Item = (), Error = ActixError>> {
+	match json {
+		Value::String(str) => {
+			Box::new(db.lock().from_err().join(future::ok(str)).and_then(move |(mut db_locked, str)| {
+				let (client, statements) = db_locked.get();
+				client.execute(&statements.add_reciever, &[&id, &str])
+					.map(|_| ()).map_err(error::ErrorInternalServerError)
+			}))
+		},
+		Value::Object(obj) => {
+			Box::new(db.lock().from_err().join(future::ok(obj)).and_then(move |(mut db_locked, obj)| {
+				let (client, statements) = db_locked.get();
+				client.execute(&statements.add_reciever, &[&id, &obj["id"].as_str()])
+					.map(|_| ()).map_err(error::ErrorInternalServerError)
+			}))
+		},
+		Value::Array(arr) => Box::new(
+			future::join_all(arr.to_owned().into_iter().map(move |el| process_recievers(el, id, db.clone()))).map(|_| ())
+				.map_err(|e| error::InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR).into())
+		),
+		_ => Box::new(future::err(error::ErrorBadRequest("Invaild actor")))
+	}
+}
+
+pub fn init(user_name: &str) -> Box<Future<Item = DbWrapper, Error = io::Error>> {
+	Box::new(
+		connect(&(String::from("postgres://") + user_name + "@localhost/graft"), NoTls)
+			.map_err(|e| io::Error::new(ErrorKind::Other, e))
+			.and_then(move |(mut cl, conn)| {
+				Arbiter::spawn(conn.map_err(|e| panic!("{}", e)));
+				future::join_all(vec![
+					// Insert SQL statements here
+					cl.prepare("SELECT * FROM messages WHERE id IN (SELECT message FROM messages_recieved WHERE actor = $1) ORDER BY ctime;"), // get inbox
+					cl.prepare("SELECT * FROM messages WHERE id IN (SELECT message FROM messages_sent WHERE actor = $1) ORDER BY ctime;"), // get outbox
+					cl.prepare("INSERT INTO messages (content, ctime, mtime) VALUES ($1, $2, $2) RETURNING id;"), // create message
+					cl.prepare("INSERT INTO messages_sent (actor, message) VALUES ($2, $1);"), // add sender
+					cl.prepare("INSERT INTO messages_recieved (actor, message) VALUES ($2, $1);"), // add reciever
+					cl.prepare("INSERT INTO actors (actortype, id) VALUES ($1, $2);"), // create actor
+					cl.prepare("DELETE FROM actors WHERE actortype = 'organization' AND id = $1;") // delete group
+				]).and_then(move |statements| {
+					println!("SQL Statements prepared successfully");
+					let mut iter = statements.into_iter();
+					Ok(web::Data::new(Mutex::new(Db {
+						client: cl,
+						statements: Statements {
+							get_inbox: iter.next().unwrap(),
+							get_outbox: iter.next().unwrap(),
+							create_message: iter.next().unwrap(),
+							add_sender: iter.next().unwrap(),
+							add_reciever: iter.next().unwrap(),
+							create_group: iter.next().unwrap(),
+							delete_group: iter.next().unwrap()
+						}
+					})))
+				}).map_err(|e| io::Error::new(ErrorKind::Other, e))
+			})
+	)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,19 @@
-use std::error::Error;
 use futures::future;
 use actix_web::{HttpServer, App, web, HttpRequest, Responder, error::{self, Error as ActixError}, dev::RequestHead};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use serde_json::{Value, Map};
 use activitypub::{actor, collection};
-use actix_web::http::{StatusCode, uri::{Uri, Parts, PathAndQuery}};
-use tokio_postgres::{connect, NoTls, Statement, Client, Row, types::{Type, Kind, IsNull, ToSql}};
-use futures_locks::Mutex;
-use std::io::{self, ErrorKind, stdin, stdout, Write};
+use actix_web::http::uri::{Uri, Parts, PathAndQuery};
+use std::io::{stdin, stdout, Write};
 use actix::prelude::*;
 use chrono::Utc;
 
-#[derive(Debug)]
-enum ActorVariant {
-	User,
-	Group
-}
-
-// Manually expanded https://github.com/sfackler/rust-postgres-derive since it didn't work with tokio-postgres
-impl ToSql for ActorVariant {
-	fn to_sql(&self, _type: &Type, buf: &mut Vec<u8>) -> Result<IsNull, Box<Error + Sync + Send>> {
-		let s = match self {
-            ActorVariant::User => "member",
-			ActorVariant::Group => "organization"
-        };
-
-        buf.extend_from_slice(s.as_bytes());
-        Ok(IsNull::No)
-    }
-
-    fn accepts(type_: &Type) -> bool {
-		if type_.name() != "actors_available" {
-            return false;
-        }
-
-        match *type_.kind() {
-            Kind::Enum(ref variants) => {
-                if variants.len() != 2 {
-                    return false;
-                }
-
-                variants.iter().all(|v| {
-                    match &**v {
-                        "member" => true,
-                        "organization" => true,
-                        _ => false
-                    }
-                })
-            }
-            _ => false
-        }
-    }
-
-    fn to_sql_checked(&self, type_: &Type, buf: &mut Vec<u8>) -> Result<IsNull, Box<Error + Sync + Send>> {
-		self.to_sql(type_, buf)
-	}
-}
-
-fn into_value(row: &Row, name: &str, col_type: &Type) -> Value {
-	macro_rules! from_sql {
-		($(($sql_type:ident, $type_to:ty)),*) => {
-			match col_type {
-				$(&Type::$sql_type => row.get::<&str, $type_to>(name).into(),)*
-				_ => panic!("Specified SQL cell's type is not compatible to JSON")
-			}
-		}
-	}
-	from_sql![
-		(CHAR, i8),
-		(INT2, i16),
-		(INT4, i32),
-		(INT8, i64),
-		(OID, u32),
-		(FLOAT4, f32),
-		(FLOAT8, f64),
-		(BYTEA, &[u8]),
-		(TEXT, &str),
-		(BOOL, bool)
-	]
-}
+mod db;
+use db::{DbWrapper, process_senders, process_recievers};
 
 fn group() -> impl Responder {
 	// Maybe a web interface
+	unimplemented!();
 }
 
 fn group_json(req: HttpRequest) -> impl Responder {
@@ -112,7 +44,7 @@ fn inbox(req: HttpRequest, db: DbWrapper) -> impl Future<Item = String, Error = 
 			.map(|row| row.columns().into_iter()
 				.map(|col| {
 					let name = col.name();
-					(String::from(name), into_value(&row, name, col.type_()))
+					(String::from(name), db::into_value(&row, name, col.type_()))
 				})
 				.collect::<Map<String, Value>>())
 			.collect().and_then(move |items| {
@@ -134,7 +66,7 @@ fn outbox(req: HttpRequest, db: DbWrapper) -> impl Future<Item = String, Error =
 			.map(|row| row.columns().into_iter()
 				.map(|col| {
 					let name = col.name();
-					(String::from(name), into_value(&row, name, col.type_()))
+					(String::from(name), db::into_value(&row, name, col.type_()))
 				})
 				.collect::<Map<String, Value>>())
 			.collect().and_then(move |items| {
@@ -148,7 +80,7 @@ fn create(req: HttpRequest, db: DbWrapper) -> impl Future<Item = &'static str, E
 	db.lock().from_err().and_then(move |mut db_locked| {
 		let (client, statements) = db_locked.get();
 		client.execute(&statements.create_group, &[
-			&ActorVariant::Group,
+			&db::ActorVariant::Group,
 			&req.match_info().query("groupname")
 		]).map(|_| "Group succesfully created").map_err(error::ErrorInternalServerError)
 	})
@@ -156,54 +88,6 @@ fn create(req: HttpRequest, db: DbWrapper) -> impl Future<Item = &'static str, E
 
 fn delete() -> impl Responder {
 	"Deleting a group is not supported"
-}
-
-fn process_senders(json: Value, id: i64, db: DbWrapper) -> Box<Future<Item = (), Error = ActixError>> {
-	match json {
-		Value::String(str) => {
-			Box::new(db.lock().from_err().join(future::ok(str)).and_then(move |(mut db_locked, str)| {
-				let (client, statements) = db_locked.get();
-				client.execute(&statements.add_sender, &[&id, &str])
-					.map(|_| ()).map_err(error::ErrorInternalServerError)
-			}))
-		},
-		Value::Object(obj) => {
-			Box::new(db.lock().from_err().join(future::ok(obj)).and_then(move |(mut db_locked, obj)| {
-				let (client, statements) = db_locked.get();
-				client.execute(&statements.add_sender, &[&id, &obj["id"].as_str()])
-					.map(|_| ()).map_err(error::ErrorInternalServerError)
-			}))
-		},
-		Value::Array(arr) => Box::new(
-			future::join_all(arr.to_owned().into_iter().map(move |el| process_senders(el, id, db.clone()))).map(|_| ())
-				.map_err(|e| error::InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR).into())
-		),
-		_ => Box::new(future::err(error::ErrorBadRequest("Invaild actor")))
-	}
-}
-
-fn process_recievers(json: Value, id: i32, db: DbWrapper) -> Box<Future<Item = (), Error = ActixError>> {
-	match json {
-		Value::String(str) => {
-			Box::new(db.lock().from_err().join(future::ok(str)).and_then(move |(mut db_locked, str)| {
-				let (client, statements) = db_locked.get();
-				client.execute(&statements.add_reciever, &[&id, &str])
-					.map(|_| ()).map_err(error::ErrorInternalServerError)
-			}))
-		},
-		Value::Object(obj) => {
-			Box::new(db.lock().from_err().join(future::ok(obj)).and_then(move |(mut db_locked, obj)| {
-				let (client, statements) = db_locked.get();
-				client.execute(&statements.add_reciever, &[&id, &obj["id"].as_str()])
-					.map(|_| ()).map_err(error::ErrorInternalServerError)
-			}))
-		},
-		Value::Array(arr) => Box::new(
-			future::join_all(arr.to_owned().into_iter().map(move |el| process_recievers(el, id, db.clone()))).map(|_| ())
-				.map_err(|e| error::InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR).into())
-		),
-		_ => Box::new(future::err(error::ErrorBadRequest("Invaild actor")))
-	}
 }
 
 fn post(json: web::Json<Value>, db: DbWrapper) -> Box<Future<Item = String, Error = ActixError>> {
@@ -229,33 +113,6 @@ fn post(json: web::Json<Value>, db: DbWrapper) -> Box<Future<Item = String, Erro
 	}
 }
 
-struct Db {
-	client: Client,
-	statements: Statements
-}
-
-impl Actor for Db {
-    type Context = Context<Self>;
-}
-
-impl Db {
-	fn get(&mut self) -> (&mut Client, &Statements) {
-		(&mut self.client, &self.statements)
-	}
-}
-
-struct Statements {
-	get_inbox: Statement,
-	get_outbox: Statement,
-	create_message: Statement,
-	add_sender: Statement,
-	add_reciever: Statement,
-	create_group: Statement,
-	delete_group: Statement
-}
-
-type DbWrapper = web::Data<Mutex<Db>>;
-
 fn main() {
     // load ssl keys
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
@@ -268,38 +125,7 @@ fn main() {
     stdin().read_line(&mut user_name).expect("Failed to read line");
 	let len = user_name.len();
 
-	let future = connect(&(String::from("postgres://") + &user_name[0..len-1] + "@localhost/graft"), NoTls)
-			.map_err(|e| io::Error::new(ErrorKind::Other, e))
-			.and_then(move |(mut cl, conn)| {
-		Arbiter::spawn(conn.map_err(|e| panic!("{}", e)));
-		future::join_all(vec![
-			// Insert SQL statements here
-			cl.prepare("SELECT * FROM messages WHERE id IN (SELECT message FROM messages_recieved WHERE actor = $1) ORDER BY ctime;"), // get inbox
-			cl.prepare("SELECT * FROM messages WHERE id IN (SELECT message FROM messages_sent WHERE actor = $1) ORDER BY ctime;"), // get outbox
-			cl.prepare("INSERT INTO messages (content, ctime, mtime) VALUES ($1, $2, $2) RETURNING id;"), // create message
-			cl.prepare("INSERT INTO messages_sent (actor, message) VALUES ($2, $1);"), // add sender
-			cl.prepare("INSERT INTO messages_recieved (actor, message) VALUES ($2, $1);"), // add reciever
-			cl.prepare("INSERT INTO actors (actortype, id) VALUES ($1, $2);"), // create actor
-			cl.prepare("DELETE FROM actors WHERE actortype = 'organization' AND id = $1;") // delete group
-		]).and_then(move |statements| {
-			let mut iter = statements.into_iter();
-			Ok(Db {
-				client: cl,
-				statements: Statements {
-					get_inbox: iter.next().unwrap(),
-					get_outbox: iter.next().unwrap(),
-					create_message: iter.next().unwrap(),
-					add_sender: iter.next().unwrap(),
-					add_reciever: iter.next().unwrap(),
-					create_group: iter.next().unwrap(),
-					delete_group: iter.next().unwrap()
-				}
-			})
-		}).map_err(|e| io::Error::new(ErrorKind::Other, e))
-	}).and_then(|db| {
-		println!("SQL Statements prepared successfully");
-
-		let db = web::Data::new(Mutex::new(db));
+	let future = db::init(&user_name[0..len-1]).and_then(|db| {
 		HttpServer::new(move || {
 			App::new()
 				.register_data(db.clone())
